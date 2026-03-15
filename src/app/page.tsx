e8e3db0c45a68474f587f615a4a411944fc016fd
TheThouZands <paulosc_10@hotmail.com>
2026-03-15 15:30:05 -0500

    random test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,35 +5,14 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={`${styles.logo} ${styles.invertOnDark}`}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className={styles.copy}>
           <h1 className={styles.title}>
-            To get started, edit the page.tsx file.
+            lmao
           </h1>
           <p className={styles.subtitle}>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className={styles.inlineLink}
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className={styles.inlineLink}
-            >
-              Learning
-            </a>{" "}
-            center.
+            lol
           </p>
+          <p>psa</p>
         </div>
         <div className={styles.actions}>
           <a
@@ -42,14 +21,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className={styles.invertOnDark}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
+            xd
           </a>
           <a
             className={`${styles.actionBase} ${styles.actionSecondary}`}
@@ -57,7 +29,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            asd
           </a>
         </div>
       </main>
