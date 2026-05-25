0f91bdee0cf4a91dbab73dbd3cd9e86eed2d9eb3
TheThouZands <paulosc_10@hotmail.com>
2026-05-24 21:33:06 -0500

    misc: serve a pdf

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,27 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/pdfs/:path*",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/pdf",
+          },
+          {
+            key: "Content-Disposition",
+            value: "inline",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
