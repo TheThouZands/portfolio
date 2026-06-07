ca98b9ce8f790f3f42ca34d526104e653901a865
TheThouZands <paulosc_10@hotmail.com>
2026-06-06 23:03:01 -0500

    feat: main hero [KAN-8]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import WhoamiHero from "@/components/heroes/whoami";
+
 export default async function Home() {
     return (
         <main>
-            thouzands
+            <WhoamiHero/>
         </main>
     );
 }
---
+++ src/components/heroes/whoami.tsx
@@ -0,0 +1,14 @@
+export default function WhoamiHero () {
+    return (
+        <section>
+            <h1>Thouzands</h1>
+            <span>Systems
+                  Stability
+                  Maintainability
+                  Responsiveness
+                  Accessibility
+                  Efficiency
+                  Tools, not just websites</span> {/* could in theory make all of this a switching text */}
+        </section>
+    );
+}
