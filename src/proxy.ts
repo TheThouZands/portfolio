ad88657f4e0dd18ab23ace6ed6bc2818d0a2b7db
TheThouZands <paulosc_10@hotmail.com>
2026-06-20 23:58:53 -0500

    fix(skills): redirect to home on empty [KAN-10]

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,6 @@
 import createMiddleware from "next-intl/middleware";
 import { type NextRequest, NextResponse } from "next/server";
+import { getCookieLocale } from "./i18n/locale";
 import { routing } from "./i18n/routing";
 
 const intlMiddleware = createMiddleware(routing);
@@ -8,7 +9,15 @@ function isBareBlogPostResolverPath(pathname: string) {
   return /^\/blog\/[^/]+\/?$/.test(pathname);
 }
 
+function isBareSkillsIndexPath(pathname: string) {
+  return /^\/skills\/?$/.test(pathname);
+}
+
 export default function proxy(request: NextRequest) {
+  if (isBareSkillsIndexPath(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL(`/${getCookieLocale(request)}`, request.url), 308);
+  }
+
   if (isBareBlogPostResolverPath(request.nextUrl.pathname)) {
     return NextResponse.next();
   }
